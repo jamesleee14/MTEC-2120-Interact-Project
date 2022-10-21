4458bd5eb38b8181b4e1699fbe33974496fd5fcb
Student <student@V120-ENT-010.local>
2022-10-21 17:12:41 -0400

    james's push

---
+++ MTEC 2120 Interact Group Project/Assets/Gravity.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Gravity : MonoBehaviour
+{
+
+    bool gravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Input(InputAction.CallbackContext context)
+    {
+        //GetComponent<Rigidbody>().
+    }
+
+}
+
+/*
+forum.unity.com/threads/new-input-system-triggers-action-on-press-and-release.1161764/
+*/
