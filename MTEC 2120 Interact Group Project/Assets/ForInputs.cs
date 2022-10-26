23b6935b62a9b6ea55a05a0ca0f5e89d8a965969
James Lee <James.Lee@mail.citytech.cuny.edu>
2022-10-26 01:00:27 -0400

    james's push

--- MTEC 2120 Interact Group Project/Assets/ForInputs.cs
+++ MTEC 2120 Interact Group Project/Assets/ForInputs.cs
@@ -9,15 +9,15 @@ public class ForInputs : MonoBehaviour
 
     public GameObject ball;
 
-    public void OnFire(InputAction.CallbackContext context)
+    public void Spawn(InputAction.CallbackContext context)
     {
         Vector3 ballPosition = new Vector3(Random.Range(-2.5f, 2.5f), Random.Range(1, 2.5f), Random.Range(-2.5f, 2.5f));
         Instantiate(ball, ballPosition, Quaternion.identity);
 
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Application.LoadLevel(0);
-        }
     }
 
+    public void Restart(InputAction.CallbackContext context)
+    {
+        Application.LoadLevel(0);
+    }
 }
